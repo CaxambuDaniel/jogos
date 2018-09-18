8747548ea7941c623d0eca9989f05e56637083f8
CaxambuDaniel <contatocarvalhos@gmail.com>
2018-09-18 12:15:08 -0300

    Aula 01 e 02 feitas

--- forca.py
+++ forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = 'banana'
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou  and not acertou):
+        chute = input('Qual a letra? ').strip()#strip() tira os espaços inseridos pelo usuario.
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.lower() == letra.lower()):#lower() deixa todas strings minuscalas para nao ter problemas com a comparação
+                print('encontrei a letra {} na posição {}'.format(chute, index+1))
+            index = index+1
+
+        print ('jogando...')
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):

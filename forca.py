1c97e78f09e8d0c74823cb88b3ceee756f58cfbc
CaxambuDaniel <contatocarvalhos@gmail.com>
2018-09-19 11:18:59 -0300

    Aula 03 feitas

--- forca.py
+++ forca.py
@@ -4,20 +4,24 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = 'banana'
+    letras_acertadas = ['_', '_', '_', '_', '_', '_', ]
+
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
 
     while(not enforcou  and not acertou):
-        chute = input('Qual a letra? ').strip()#strip() tira os espaços inseridos pelo usuario.
+        chute = input('Qual a letra? ').strip()#
+
 
         index = 0
         for letra in palavra_secreta:
-            if (chute.lower() == letra.lower()):#lower() deixa todas strings minuscalas para nao ter problemas com a comparação
-                print('encontrei a letra {} na posição {}'.format(chute, index+1))
+            if (chute.lower() == letra.lower()):#lower() deixa todas strings minuscalas
+                letras_acertadas[index]=letra
             index = index+1
 
-        print ('jogando...')
+        print (letras_acertadas)
 
     print("Fim do jogo")
 
